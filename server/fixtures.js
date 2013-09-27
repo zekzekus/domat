b50db3d8c216402de8f120c0cd6ffe3316827d7b
Zekeriya Koc <zekzekus@gmail.com>
2013-09-27 21:13:44 +0300

    new task fields added

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,11 +2,13 @@ if (Tasks.find().count() === 0) {
     Tasks.insert({
         description: 'implement secure routing',
         assignee: 'zekus',
-        completed_pomodoros: 3
+        completed_pomodoros: 3,
+        completed: false
     });
     Tasks.insert({
         description: 'prepare product documentation',
         assignee: 'hasan',
-        completed_pomodoros: 0
+        completed_pomodoros: 0,
+        completed: true
     });
 }
