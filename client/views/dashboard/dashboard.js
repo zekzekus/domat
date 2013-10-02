8cf83da792d73dead419f9a2237acc2dd43c6676
Zekeriya Koc <zekzekus@gmail.com>
2013-10-02 22:02:28 +0300

    timer value formatted for presentation

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,29 +1,43 @@
+var secondsToTime = function(sec) {
+    var remainingMinutes = Math.floor(sec / 60);
+    var remainingSeconds = sec - remainingMinutes * 60;
+    var digit = "";
+    if (remainingSeconds < 10) {
+        digit = "0";
+    }
+    return remainingMinutes.toString() + ':' + digit + String(remainingSeconds);
+};
+
 Template.dashboard.helpers({
     timerValue: function() {
         return Session.get('timer');
     }
 });
 
-
+var seconds = 25 * 60;
 Meteor.startup(function() {
-    Session.set('timer', 25);
+    Session.set('seconds', seconds);
+    Session.set('timer', secondsToTime(seconds));
 });
+
 
 Template.dashboard.events({
     'click #btn-reset': function(e) {
-        Session.set('timer', 25);
+        Session.set('seconds', seconds);
+        Session.set('timer', secondsToTime(seconds));
         Meteor.clearInterval(Session.get('timerHandle'));
     },
     'click #btn-start': function(e) {
         var timerHandle = Meteor.setInterval(function() {
-            currentVal = Session.get('timer');
+            currentVal = Session.get('seconds');
             if (currentVal > 0) {
-                Session.set('timer', currentVal - 1);
+                Session.set('seconds', currentVal - 1);
+                Session.set('timer', secondsToTime(currentVal));
             } else {
                 Meteor.clearInterval(Session.get('timerHandle'));
                 return;
             }
-        }, 1000);
+        }, 100);
         Session.set('timerHandle', timerHandle);
     }
 });
