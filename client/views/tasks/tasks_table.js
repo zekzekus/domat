1a2f1e8e2e6665bf0e489cf13c1a86179b929bf7
Zekeriya Koc <zekzekus@gmail.com>
2013-10-05 20:42:11 +0300

    implemented task inline editing of task description completely

--- client/views/tasks/tasks_table.js
+++ client/views/tasks/tasks_table.js
@@ -1,3 +1,31 @@
+// Returns an event map that handles the "escape" and "return" keys and
+// "blur" events on a text input (given by selector) and interprets them
+// as "ok" or "cancel".
+var okCancelEvents = function (selector, callbacks) {
+  var ok = callbacks.ok || function () {};
+  var cancel = callbacks.cancel || function () {};
+
+  var events = {};
+  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
+    function (evt) {
+      if (evt.type === "keydown" && evt.which === 27) {
+        // escape = cancel
+        cancel.call(this, evt);
+
+      } else if (evt.type === "keyup" && evt.which === 13 ||
+                 evt.type === "focusout") {
+        // blur/return/enter = ok/submit if non-empty
+        var value = String(evt.target.value || "");
+        if (value)
+          ok.call(this, value, evt);
+        else
+          cancel.call(this, evt);
+      }
+    };
+
+  return events;
+};
+
 Template.tasksTable.helpers({
     tasks: function() {
         return Tasks.find({}, {sort: {timestamp: -1}});
@@ -31,18 +59,30 @@ Template.taskItem.events({
     'click #btn-delete': function(e) {
         Tasks.remove(this._id);
     },
-    'dblclick .task-description': function(e) {
-        Session.set('editing', true);
-    },
-    'keyup .txt-edit-task': function(e) {
-        if (e.which === 13) {
-            Session.set('editing', false);
-        }
+    'dblclick .task-description': function(e, t) {
+        Session.set('editing_task', this._id);
+        Deps.flush();
+        t.find('#task-input').focus();
+        t.find('#task-input').select();
     }
 });
 
+Template.taskItem.events(okCancelEvents(
+    '#task-input',
+    {
+        ok: function(value) {
+            Tasks.update(this._id, {$set: {description: value}});
+            Session.set('editing_task', null);
+        },
+        cancel: function() {
+            Session.set('editing_task', null);
+        }
+    }
+));
+
 Template.taskItem.helpers({
     editing: function() {
-        return Session.get('editing');
+        return Session.equals('editing_task', this._id);
     }
 });
+
