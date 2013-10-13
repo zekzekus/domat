703afe67c33f4c396ca6b5b64964e345fea52a16
Zekeriya Koc <zekzekus@gmail.com>
2013-10-13 20:36:12 +0300

    fixed initialization point of the pomodoro timer

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,11 +1,28 @@
-Template.dashboard.helpers({
-    timerValue: function() {
-        return Session.get('timer');
-    }
-});
+pomodoro = undefined;
 
 Template.dashboard.events({
     'click #btn-pomodoro': function(e) {
+        if (pomodoro === undefined) {
+            pomodoro = new Pomodoro({
+                workDuration: Session.get('tmpWork'),
+                shortBreakDuration: Session.get('tmpShort'),
+                longBreakDuration: Session.get('tmpLong'),
+                onCountdown: function() {
+                    Session.set('timer', this.getPrettyTime());
+                    Session.set('percent', this.getPercent());
+                    Session.set('state', this.getPrettyState());
+                },
+                onWorkFinish: function() {
+                    var linked_id = Session.get('linked_id');
+                    if (linked_id !== undefined) {
+                        var linked_task = Tasks.findOne({_id: linked_id});
+                        Tasks.update(linked_id, {$set: {
+                            completed_pomodoros: linked_task.completed_pomodoros + 1
+                        }});
+                    }
+                }
+            });
+        }
         $('#myModal').modal({keyboard: false});
     }
 });
